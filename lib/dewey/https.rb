988be17b017a2aba4941abee5ddcef8371230f7c
Parker Selbert <parker@sorentwo.com>
2010-09-13 08:27:20 -0500

    Prevent peer certificate warning

--- lib/dewey/https.rb
+++ lib/dewey/https.rb
@@ -1,11 +1,12 @@
 require 'net/http'
 
 module Net # :nodoc:
-  # Shortcut method for using +http.use_ssl+.
   class HTTPS < Net::HTTP
     def initialize(address, port = 443)
       super(address, port)
       self.use_ssl = true
+      @ssl_context = OpenSSL::SSL::SSLContext.new
+      @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
     end
   end
 end
