c9ed571173a1eb41eb9dd542c0189cf025757d90
Parker Selbert <parker@sorentwo.com>
2010-10-28 07:07:36 -0500

    Better documentation for Validation. Also handle either string or symbol when checking formats

--- lib/dewey/validation.rb
+++ lib/dewey/validation.rb
@@ -1,20 +1,28 @@
 module Dewey
   
-  DOCUMENT_EXPORT_FORMATS     = %w(txt odt pdf html rtf doc png zip)
-  PRESENTATION_EXPORT_FORMATS = %w(swf pdf png ppt)
-  SPREADSHEET_EXPORT_FORMATS  = %W(xls csv pdf ods tsv html)
+  DOCUMENT_EXPORT_FORMATS     = [:txt, :odt, :pdf, :html, :rtf, :doc, :png, :zip]
+  PRESENTATION_EXPORT_FORMATS = [:swf, :pdf, :png, :ppt]
+  SPREADSHEET_EXPORT_FORMATS  = [:xls, :csv, :pdf, :ods, :tsv, :html]
   
+  # Utility methods to check that a format is accepted for a particular service
+  #
   class Validation
     # Determine wether or not a format is available for download.
     #
-    # * format  - The file format to check, i.e. 'txt', 'doc', 'pdf'
-    # * service - The service that would be used. Must be document, presentation,
-    #   or spreadsheet.
+    # @param [Symbol] format  The file format to check, i.e. `:txt`, `:doc`, `:pdf`
+    # @param [Symbol] service The service that would be used. Must be
+    #   `:document`, `:presentation`, or `:spreadsheet`.
+    #
+    # @return [Boolean] `true` if the format is supported, `false` otherwise
+    #
+    # @raise [DeweyException] Raised when an unknown service is given
     def self.valid_upload_format?(format, service = :document)
+      format = format.to_sym
+
       case service
-      when :document     then Dewey::DOCUMENT_MIMETYPES.has_key?(format.to_sym)
-      when :presentation then Dewey::PRESENTATION_MIMETYPES.has_key?(format.to_sym)
-      when :spreadsheet  then Dewey::SPREADSHEET_MIMETYPES.has_key?(format.to_sym)
+      when :document     then Dewey::DOCUMENT_MIMETYPES.has_key?(format)
+      when :presentation then Dewey::PRESENTATION_MIMETYPES.has_key?(format)
+      when :spreadsheet  then Dewey::SPREADSHEET_MIMETYPES.has_key?(format)
       else
         raise DeweyException, "Unknown service: #{service}"
       end
@@ -22,10 +30,16 @@ module Dewey
 
     # Determine whether or not a format is available for export.
     #
-    # * format  - The file format to check, i.e. 'txt', 'doc', 'pdf'
-    # * service - The service that would be used. Must be document, presentation,
-    #   or spreadsheet.
+    # @param [Symbol] format  The file format to check, i.e. `:txt`, `:doc`, `:pdf`
+    # @param [Symbol] service The service that would be used. Must be
+    #   `:document`, `:presentation`, or `:spreadsheet`.
+    #
+    # @return [Boolean] `true` if the format is supported, `false` otherwise
+    #
+    # @raise [DeweyException] Raised when an unknown service is given
     def self.valid_export_format?(format, service = :document)
+      format = format.to_sym
+
       case service
       when :document     then Dewey::DOCUMENT_EXPORT_FORMATS.include?(format)
       when :presentation then Dewey::PRESENTATION_EXPORT_FORMATS.include?(format)
--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -3,34 +3,34 @@ require 'spec_helper'
 describe Dewey::Validation do
   
   it "should return true for valid upload formats" do
-    Dewey::Validation.valid_upload_format?('txt', :document).should be_true
-    Dewey::Validation.valid_upload_format?('ppt', :presentation).should be_true
-    Dewey::Validation.valid_upload_format?('csv', :spreadsheet).should be_true
+    Dewey::Validation.valid_upload_format?(:txt, :document).should be_true
+    Dewey::Validation.valid_upload_format?(:ppt, :presentation).should be_true
+    Dewey::Validation.valid_upload_format?(:csv, :spreadsheet).should be_true
   end
 
   it "should return false for invalid upload formats" do
-    Dewey::Validation.valid_upload_format?('png', :document).should_not be_true
-    Dewey::Validation.valid_upload_format?('txt', :presentation).should_not be_true
-    Dewey::Validation.valid_upload_format?('pdf', :spreadsheet).should_not be_true
+    Dewey::Validation.valid_upload_format?(:png, :document).should_not be_true
+    Dewey::Validation.valid_upload_format?(:txt, :presentation).should_not be_true
+    Dewey::Validation.valid_upload_format?(:pdf, :spreadsheet).should_not be_true
   end
 
   it "should raise when given an invalid upload service" do
-    lambda { Dewey::Validation.valid_upload_format?('ical', :cl) }.should raise_exception(Dewey::DeweyException)
+    lambda { Dewey::Validation.valid_upload_format?(:ical, :cl) }.should raise_exception(Dewey::DeweyException)
   end
 
   it "should return true for valid export formats" do
-    Dewey::Validation.valid_export_format?('txt', :document).should be_true
-    Dewey::Validation.valid_export_format?('pdf', :presentation).should be_true
-    Dewey::Validation.valid_export_format?('csv', :spreadsheet).should be_true
+    Dewey::Validation.valid_export_format?(:txt, :document).should be_true
+    Dewey::Validation.valid_export_format?(:pdf, :presentation).should be_true
+    Dewey::Validation.valid_export_format?(:csv, :spreadsheet).should be_true
   end
 
   it "should return false for invalid export formats" do
-    Dewey::Validation.valid_export_format?('jpg', :document).should be_false
-    Dewey::Validation.valid_export_format?('txt', :presentation).should be_false
-    Dewey::Validation.valid_export_format?('txt', :spreadsheet).should be_false
+    Dewey::Validation.valid_export_format?(:jpg, :document).should be_false
+    Dewey::Validation.valid_export_format?(:txt, :presentation).should be_false
+    Dewey::Validation.valid_export_format?(:txt, :spreadsheet).should be_false
   end
 
   it "should raise when given an invalid export service" do
-    lambda { Dewey::Validation.valid_export_format?('ical', :cl) }.should raise_exception(Dewey::DeweyException)
+    lambda { Dewey::Validation.valid_export_format?(:ical, :cl) }.should raise_exception(Dewey::DeweyException)
   end
 end
