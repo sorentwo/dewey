619a625ac40de5be55063dacf80a31b7bc103e53
Parker Selbert <parker@sorentwo.com>
2010-11-14 21:19:02 -0600

    Integer conversion is necessary

--- lib/dewey/utils.rb
+++ lib/dewey/utils.rb
@@ -3,7 +3,7 @@ module Dewey
     # Perform string escaping for Atom slugs
     def slug(string)
       string.chars.to_a.map do |char|
-        decimal = char.unpack('U').join('')
+        decimal = char.unpack('U').join('').to_i
         if decimal < 32 || decimal > 126 || decimal == 37
           char = "%#{char.unpack('H2').join('%').upcase}"
         end
