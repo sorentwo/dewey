13977d0db095adaed83641f7965c596c42c75bc3
Cory Schires <coryschires@gmail.com>
2011-09-25 19:20:36 -0500

    Allow file extension as an option on `Dewey.put`

--- lib/dewey/core.rb
+++ lib/dewey/core.rb
@@ -5,7 +5,7 @@ require 'tempfile'
 
 module Dewey
   class << self
-    
+
     # Set the authentication strategy. You can currently only use ClientAuth,
     # but you must provide +email+ and +password+ options. You must set up
     # authentication before using any file operations.
@@ -16,14 +16,14 @@ module Dewey
         @@authenticator = Dewey::ClientAuth.new(options[:email], options[:password])
       end
     end
-    
+
     # Report whether Dewey has both an authentication strategy selected, and has
     # been authenticated. Delegates to the chosen authenticator.
     #
     def authenticated?
       !@@authenticator.nil? && @@authenticator.authenticated?
     end
-    
+
     # Tell the authentication strategy to authenticate. Not necessary though, as
     # any file operation will authenticate automatically.
     #
@@ -31,7 +31,7 @@ module Dewey
       @@authenticator.authenticate!
     end
 
-    # Queries the document list for a particular title. Titles can be either 
+    # Queries the document list for a particular title. Titles can be either
     # full or partial matches. Matches are returned as an array of ids.
     #
     # @param [String] query   The title to be matched
@@ -46,7 +46,7 @@ module Dewey
       url      = "#{GOOGLE_FEED_URL}?title=#{title}"
       url     << "&title-exact=true" if options[:exact]
       response = http_request(:get, url, headers)
-      
+
       response.kind_of?(Net::HTTPOK) ? extract_ids(response.body) : []
     end
 
@@ -57,27 +57,27 @@ module Dewey
     # @param [Hash] options Options for uploading the file
     # @option options [Symbol] :title An alternative title, to be used instead
     #                                 of the filename
-    # 
+    #
     # @return [String, nil] The id if upload was successful, nil otherwise
     def put(file, options = {})
-      extension = File.extname(file.path).sub('.', '')
+      extension = options[:extension] || File.extname(file.path).sub('.', '')
       basename  = File.basename(file.path, ".#{extension}")
       mimetype  = Dewey::Mime.mime_type(file)
       service   = Dewey::Mime.guess_service(mimetype)
       title     = options[:title] || basename
-  
+
       raise DeweyError, "Invalid file type: #{extension}" unless Dewey::Validation.valid_upload_format?(extension, service)
-  
+
       headers = base_headers
       headers['Content-Length'] = File.size?(file).to_s
       headers['Slug']           = Dewey::Utils.slug(title)
       headers['Content-Type']   = mimetype unless mimetype =~ /Can't expand summary_info/
-  
+
       # Rewind the file in the case of multiple uploads, or conversions
       file.rewind
-  
+
       response = http_request(:post, GOOGLE_FEED_URL, headers, file.read.to_s)
- 
+
       if response.kind_of?(Net::HTTPCreated)
         extract_ids(response.body).first
       else
@@ -98,7 +98,7 @@ module Dewey
     #                          `My Document` for example
     # @param [Hash]   options  Options for downloading the document
     # @option options [Symbol] :format The output format
-    # 
+    #
     # @return [Tempfile, nil] The downloaded file, otherwise `nil` if the file
     #                         couldn't be found.
     #
@@ -137,12 +137,12 @@ module Dewey
           url << "&format=#{format}"       if service == 'document'
           url << "&gid=#{options[:sheet]}" if service == 'spreadsheet' && options[:sheet]
         else
-          raise DeweyError, "Invalid format: #{format}" 
+          raise DeweyError, "Invalid format: #{format}"
         end
       end
 
       response = http_request(:get, url, base_headers(service))
-      
+
       if response.kind_of?(Net::HTTPOK)
         file = Tempfile.new([id, format].join('.')).binmode
         file.write(response.body)
@@ -154,7 +154,7 @@ module Dewey
     end
 
     # Deletes a document. The default behavior is to delete the document
-    # permanently, rather than trashing it. 
+    # permanently, rather than trashing it.
     #
     # @param [String] query    A resource id or title. If a title is provided
     #                          a search will be performed automatically.
@@ -166,23 +166,23 @@ module Dewey
     def delete(query, options = {})
 
       # We use 'If-Match' => '*' to make sure we delete regardless of others
-      headers = base_headers.merge({'If-Match' => '*'}) 
+      headers = base_headers.merge({'If-Match' => '*'})
       trash   = options.delete(:trash) || false
       id      = (is_id?(query)) ? query : search(query, :exact => true).first
-      
+
       return false if id.nil?
 
       url   = "#{GOOGLE_FEED_URL}/#{URI.escape(id)}"
       url << "?delete=true" unless trash
 
       response = http_request(:delete, url, headers)
-  
+
       response.kind_of?(Net::HTTPOK)
     end
-    
+
     # The same as delete, except that it will raise `Dewey::DeweyError` if
     # the request fails.
-    # 
+    #
     # @see #delete
     def delete!(query, options = {})
       delete(query, options) || raise(DeweyError, "Unable to delete document")
@@ -204,9 +204,9 @@ module Dewey
     def convert(file, options = {})
       id = put(file, options[:title])
       converted = get(id, options[:format])
-  
+
       delete(id)
-  
+
       converted
     end
 
@@ -218,7 +218,7 @@ module Dewey
     end
 
     # Perform the actual request heavy lifting
-    def http_request(method, url, headers, data = nil) #:nodoc:      
+    def http_request(method, url, headers, data = nil) #:nodoc:
       url = URI.parse(url) if url.kind_of? String
 
       connection = (url.scheme == 'https') ? Net::HTTPS.new(url.host, url.port) : Net::HTTP.new(url.host, url.port)
@@ -243,7 +243,7 @@ module Dewey
       base = {}
       base['GData-Version'] = '3.0'
       base['Authorization'] = "GoogleLogin auth=#{authenticator.token(service)}"
-  
+
       base
     end
 
